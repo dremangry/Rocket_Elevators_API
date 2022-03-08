c4c42a35a5d815bd0858bda44680db44bcab2325
Felicia H <feli95angeline@gmail.com>
2022-03-08 18:48:16 -0500

    quote, residential, and commercial htmls added - TODO: style to rails

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "home#index"
+  get "home/commercial" 
+  get "home/residential" 
+  get "home/quote" 
+  
 end
