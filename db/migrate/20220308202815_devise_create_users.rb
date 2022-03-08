cffcd6091a033b78ab7b0162ff718f43fa2a8357
BrianNieckarz <guangxi@Fishi.localdomain>
2022-03-08 15:45:12 -0500

    Fixed devise migration

--- db/migrate/20220308202815_devise_create_users.rb
+++ db/migrate/20220308202815_devise_create_users.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class DeviceCreateUsers < ActiveRecord::Migration[5.2]
+class DeviseCreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       ## Database authenticatable
@@ -16,7 +16,7 @@ class DeviceCreateUsers < ActiveRecord::Migration[5.2]
 
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
-      # t.datetime :current_sign_in_at
+      # t.datetime :current_sign_in_at 
       # t.datetime :last_sign_in_at
       # t.string   :current_sign_in_ip
       # t.string   :last_sign_in_ip
