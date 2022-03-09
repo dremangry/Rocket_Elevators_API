c8b10db63108cc2aef0825661f4ff9ecfc7aff7f
Fishi <briannieckarz@gmail.com>
2022-03-09 10:25:17 -0500

    progress from tuesday

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(first_name: 'Mathieu', last_name: 'Houde', email: 'mathieu.houde@codeboxx.biz')
+User.create(first_name: 'Patrick', last_name: 'Thibault', email: 'patrick.thibault@codeboxx.biz')
+User.create(first_name: 'Francis', last_name: 'Patry-Jessop', email: 'francis.patry-jessop@codeboxx.biz')
+User.create(first_name: 'Abdul', last_name: 'Akeeb', email: 'abdul.akeeb@codebozz.biz')
+User.create(first_name: 'Thomas', last_name: 'Carrier', email: 'thomas.carrier@codeboxx.biz')
+User.create(first_name: 'Timothy', last_name: 'Kleinerman', email: 'kiril.kleinerman@codeboxx.biz')
+User.create(first_name: 'Kiril', last_name: 'Derilus', email: 'emmanuela.derilus@codeboxx.biz')
+User.create(first_name: 'Emmanuela', last_name: 'Houde', email: 'mathieu.houde@codeboxx.biz')
+User.create(first_name: 'Marie-Ève', last_name: 'Goupil', email: 'marie-eve.goupil@codeboxx.biz')
+User.create(first_name: 'Perry', last_name: 'Sawatsky', email: 'perry.sawatsky@codeboxx.biz')
