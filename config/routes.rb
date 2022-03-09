0bdb712f27deba26934802c0f07f49c16425468e
Goddraw <coolo_ricky@hotmail.com>
2022-03-09 16:01:40 -0500

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root "home#index"
-  get "home/commercial" 
-  get "home/residential" 
-  get "home/quote"
+  get "commercial", to: "home#commercial" 
+  get "residential", to: "home#residential" 
+  get "quote", to: "home#quote"
 end
