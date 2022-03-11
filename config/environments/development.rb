c75e3ad7794bf2fb4a649b8a968be4d476bacaa2
MathieuHoude <mathieu.houde@codeboxx.biz>
2022-03-10 19:22:49 -0500

    remove hosts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-  config.hosts << "thisisrocketelevators.tk"
-
 end
