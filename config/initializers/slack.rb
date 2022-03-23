b7afe469abc4c49e12052ccfa8ad8db46623b817
BrianNieckarz <briannieckarz@gmail.com>
2022-03-22 20:24:56 -0400

    commented out slack.rb

--- config/initializers/slack.rb
+++ config/initializers/slack.rb
@@ -1,11 +1,11 @@
-require 'slacknotifier'
+# require 'slacknotifier'
 
-SLACK_NOTIFIER = Slack::Notifier.new "https://hooks.slack.com/services/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", 
-channel: "#slack-notifier-api", username: "RocketElevators"
+# SLACK_NOTIFIER = Slack::Notifier.new "https://hooks.slack.com/services/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", 
+# channel: "#slack-notifier-api", username: "RocketElevators"
 
-SLACK_NOTIFIER.ping "Inside slack.rb", channel: "#slack-notifier-api"
+# SLACK_NOTIFIER.ping "Inside slack.rb", channel: "#slack-notifier-api"
 
-# message = "[check](https://www.google.ca) it <a href='https://www.google.ca'>out</a>", channel: "#slack-notifier-api"
-# Slack::Notifier::Util::LinkFormatter.format(message)
-# message2 = "<!slack-notifier-api> Posting on slack channel"
-# SLACK_NOTIFIER.ping message2
+# # message = "[check](https://www.google.ca) it <a href='https://www.google.ca'>out</a>", channel: "#slack-notifier-api"
+# # Slack::Notifier::Util::LinkFormatter.format(message)
+# # message2 = "<!slack-notifier-api> Posting on slack channel"
+# # SLACK_NOTIFIER.ping message2
