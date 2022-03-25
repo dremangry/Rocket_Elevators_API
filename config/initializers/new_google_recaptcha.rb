e7931b60ea3d093c1ee13569445d3843e666975b
DavidPelletier <dav.pel@hotmail.com>
2022-03-25 14:47:18 -0400

    change variable names for recaptcha

--- config/initializers/new_google_recaptcha.rb
+++ config/initializers/new_google_recaptcha.rb
@@ -1,7 +1,7 @@
 if Object.const_defined?('NewGoogleRecaptcha')
   NewGoogleRecaptcha.setup do |config|
-    config.site_key   = ENV["SITE_KEY_RECAPTCHA"]
-    config.secret_key = ENV["SECRET_KEY_RECAPTCHA"]
+    config.site_key   = ENV["RECAPTCHA_SITE_KEY"]
+    config.secret_key = ENV["RECAPTCHA_SECRET_KEY"]
     config.minimum_score = 0.5
   end
 end
