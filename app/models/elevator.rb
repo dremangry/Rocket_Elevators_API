c47cf1168d11cf8adee55655dee20905b7a998a0
Felicia H <feli95angeline@gmail.com>
2022-03-24 21:30:10 -0400

    algo fixed for dropbox and hash

--- app/models/elevator.rb
+++ app/models/elevator.rb
@@ -2,16 +2,13 @@ require 'slack-notifier'
 
 class Elevator < ApplicationRecord
   belongs_to :column, dependent: :destroy
-  # after_update :statusChange
 
-  
   before_update do
     if self.status_changed? == true
       @old_status = self.status_was
       statusChange
     end
   end
-
 
   def statusChange
     if self.status == "valid"
@@ -21,8 +18,7 @@ class Elevator < ApplicationRecord
 
     else self.status == "invalid"
       slacknotifier = Slack::Notifier.new(ENV["SLACK_NOTIFIER_URL"])
-      slacknotifier.ping "The Elevator #{self.id} with Serial Number #{self.serial_number} changed status fromm #{@old_status} to #{self.status}", channel: "#slack-notifier-api", username: "RocketElevators"
-      # , icon_emoji: ':rocket:'
+      slacknotifier.ping "The Elevator #{self.id} with Serial Number #{self.serial_number} changed status from #{@old_status} to #{self.status}", channel: "#slack-notifier-api", username: "RocketElevators"
     end
   end
 
