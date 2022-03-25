11c105e51d43a3d12f3f41da4aa51adf1610f3bf
Felicia H <feli95angeline@gmail.com>
2022-03-25 15:52:16 -0400

    warehouse revised

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -32,22 +32,6 @@ RailsAdmin.config do |config|
     index                   # mandatory
     root
     map
-    briefing do
-      controller do
-        # before_action :hi
-        proc do
-          p ">>>> HEYYYYYYYY"
-          
-          text = "Greetings #{current_user.first_name}. There are currently #{Elevator.all.count} elevators deployed in the #{Building.all.count} buildings of your #{Customer.all.count} customers. Currently, #{Elevator.where(status: "invalid").count} are not in Running Status and are being serviced. You currently have #{Quote.all.count} awaiting for processing and #{Lead.where(date_of_contact_request: nil).count} in your contact requests. #{Battery.all.count} batteries are deployed accross #{Address.where(entity: "building").distinct.count(:city)} cities"
-
-          client = Aws::Polly::Client.new
-          resp = client.synthesize_speech({output_format: "mp3", sample_rate: "8000", text: "Hello world!", text_type: "text", voice_id: "Matthew",})
-          audio = resp.audio_stream
-          IO.copy_stream(audio, "hello.mp3") 
-          send_file "hello.mp3", type: "audio/mpeg", disposition: 'inline'
-        end
-      end
-    end
     new
     export
     bulk_delete
--- lib/rails_admin/custom_actions.rb
+++ lib/rails_admin/custom_actions.rb
@@ -1,11 +1,9 @@
 require 'rails_admin/config/actions'
 require 'rails_admin/config/actions/base'
-require_relative 'polly_helper'
+
 module RailsAdmin
     module Config
         module Actions
-            # include PollyHelper
-
             class Root < RailsAdmin::Config::Actions::Base
             RailsAdmin::Config::Actions.register(self)
                 register_instance_option :root do
@@ -15,7 +13,6 @@ module RailsAdmin
                     'fa fa-bar-chart' # use any of font-awesome icons
                 end
             end
-            
             class Map < RailsAdmin::Config::Actions::Base
                 RailsAdmin::Config::Actions.register(self)
                 register_instance_option :root do
@@ -25,44 +22,7 @@ module RailsAdmin
                     'fa fa-map-marker' # use any of font-awesome icons
                 end
             end
-        
-            class Briefing < RailsAdmin::Config::Actions::Base
-                # include PollyHelper
-                RailsAdmin::Config::Actions.register(self)
-                register_instance_option :root do
-                    true
-                end
-                register_instance_option :link_icon do
-                    'fa fa-audio' # use any of font-awesome icons
-                end
-                
-                # register_instance_option :http_methods do
-                #     [:get]
-                # end
-         
-                # register_instance_option :controller do
-                #     before_action :hi
-                #     proc do
-                #         p ">>>> HEYYYYYYYY"
-                #         client = Aws::Polly::Client.new
-                #         resp = client.synthesize_speech({output_format: "mp3", sample_rate: "8000", text: "Hello world!", text_type: "text", voice_id: "Matthew",})
-                #         audio = resp.audio_stream
-                #         IO.copy_stream(audio, "hello.mp3") 
-                #         send_file "hello.mp3", type: "audio/mpeg", disposition: 'inline'
-                #         # send_data resp.audio_stream.read, type: "audio/mpeg", disposition: 'inline'
-
-                #     end
-
-                 
-                # end
-
-                private
-
-                def hi
-                    p ">>>>>> hiiiiii"
-                end
-            end
-
+               
         end
     end
 end
